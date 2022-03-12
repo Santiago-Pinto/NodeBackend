d0ac1d755f54262fabdeeb8285c55f63d12c3a28
Santiago <santiago.pinto@digitalbeings.net>
2022-03-12 19:20:28 -0300

    Added capability of changing the port the server is listening on using env variables

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import express, { Request, Response } from "express";
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response): void => {
   res.json({ message: "Hello World !" });
 });
 
-app.listen(port, (): void => {
-  console.log("Server Alive !");
+app.listen(PORT, (): void => {
+  console.log(`Server listening on port: ${PORT}`);
 });
