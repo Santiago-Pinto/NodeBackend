029e00699a39cd9c703f48650a205802b8c29031
Santiago <santiago.pinto@digitalbeings.net>
2022-06-26 14:45:32 -0300

    Created config file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Application } from "express";
 import { TestRouter } from "../src/api/routes/testRoutes";
 import { AlbumRouter } from "../src/api/routes/albumRoutes";
+import { Sequelize } from "sequelize";
 
 const app: Application = express();
 const PORT = process.env.PORT || 3000;
@@ -9,6 +10,13 @@ const PORT = process.env.PORT || 3000;
 app.use(TestRouter);
 app.use("/album", AlbumRouter); //This one already handles all calls with the /album prefix
 ///
+
+//DB connection
+const sequelize = new Sequelize("database", "username", "password", {
+  host: process.env.HOST || "localhost",
+  dialect: "postgres" /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */,
+});
+//
 
 app.listen(PORT, (): void => {
   console.log(`Server listening on port: ${PORT}`);
