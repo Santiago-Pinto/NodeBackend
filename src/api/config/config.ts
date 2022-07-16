fca89e60de1805b09d1576b698e461f10bdcc946
Santiago <santiago.pinto@digitalbeings.net>
2022-07-16 19:27:59 -0300

    Added config to remove data base credentials from codebase

--- src/api/config/config.ts
+++ src/api/config/config.ts
@@ -1,7 +1,15 @@
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({
+  path: path.resolve(__dirname, `${process.env.NODE_ENV}.env`),
+});
+
 export const config = {
-  HOST: "localhost",
-  DB_URI:
-    process.env.URI ||
-    "postgres://localhost:development940205@localhost:5432/Node-Backend-DB",
   NODE_ENV: process.env.NODE_ENV || "development",
+  HOST: process.env.HOST || "localhost",
+  PORT: process.env.PORT || 3000,
+  DB_URI:
+    process.env.DB_URI ||
+    `postgres://localhost:${process.env.DB_PASS}@localhost:5432/Node-Backend-DB`,
 };
